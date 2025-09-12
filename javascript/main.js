835cff8e18872cb4374425c731586c994fd25599
Rowan Laffeber <9026051@student.zadkine.nl>
2025-09-12 15:01:08 +0200

    v0.0.1
    smaller and bigger coockie when clicking

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,28 @@
-document.getElementById("cookie").onclick = function() {Counter()};
-
 let count = 0;
+const cookie = document.getElementById('cookie');
+const counter = document.getElementById('counter');
 
-function Counter() {
+// add onto counter onclick
+cookie.onclick = () => {
     count++;
-    document.getElementById('counter').textContent = count;
-}
+    counter.textContent = count;
+};
+
+// bigger ans smaller cookie when clicking
+const shrinkCookie = () => {
+    cookie.style.transition = 'transform 0.1s';
+    cookie.style.transform = 'scale(0.825)';
+};
+const growCookie = () => {
+    cookie.style.transition = 'transform 0.1s';
+    cookie.style.transform = 'scale(1)';
+};
+
+cookie.addEventListener('mouseup', growCookie);
+cookie.addEventListener('mouseleave', growCookie);
+cookie.addEventListener('mousedown', shrinkCookie);
+
+cookie.addEventListener('touchstart', shrinkCookie);
+cookie.addEventListener('touchend', growCookie);
+cookie.addEventListener('touchcancel', growCookie);
+
