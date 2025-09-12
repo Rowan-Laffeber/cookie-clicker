1ba3b09777b5ca6817440af21bc7f548096bb31f
Rowan Laffeber <9026051@student.zadkine.nl>
2025-09-12 14:42:12 +0200

    v0.0.1
    changed the tab name

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" href="assets/pictures/Cookie.png">
+    <title>cookieclicker</title>
     <link rel="stylesheet" href="assets/css-files/main.css">
 </head>
 <body>
     <div class="cookieNcounter">
         <p class="counter" id="counter">0</p>
-        <p>hello</p>
         <img id="cookie" src="assets/pictures/Cookie.png" alt="cookie">
     </div>
 </body>
