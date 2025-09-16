8057a25404f508b138f3e8c3762e9ec7bd95c9f2
Rowan Laffeber <9026051@student.zadkine.nl>
2025-09-16 10:57:43 +0200

    V0.0.1
    added base design for side menus

--- index.php
+++ index.php
@@ -8,9 +8,48 @@
     <link rel="stylesheet" href="assets/css-files/main.css">
 </head>
 <body>
+    <div class="sidemenus">
+        <div class="side-header">
+            <p>themes</p>
+        </div>
+        <div class="themes">
+            <div>
+                <img src="assets/pictures/Cookie.png" alt="cookie">
+                <p>theme 1</p>
+            </div>
+            <div>
+                <img src="assets/pictures/Cookie.png" alt="cookie">
+                <p>theme 2</p>
+            </div>
+            <div>
+                <img src="assets/pictures/Cookie.png" alt="cookie">
+                <p>theme 3</p>
+            </div>
+            <div>
+                <img src="assets/pictures/Cookie.png" alt="cookie">
+                <p>theme 4</p>
+            </div>
+        </div>
+    </div>
     <div class="cookieNcounter">
         <p class="counter" id="counter">0</p>
         <img id="cookie" src="assets/pictures/Cookie.png" alt="cookie">
+    </div>
+    <div class="sidemenus">
+    <div class="side-header">
+            <p>upgrades</p>
+        </div>
+        <div class="upgrades">
+            <img class="item1" src="assets/pictures/Cookie.png" alt="">
+            <img class="item2" src="assets/pictures/Cookie.png" alt="">
+            <img class="item3" src="assets/pictures/Cookie.png" alt="">
+            <img class="item4" src="assets/pictures/Cookie.png" alt="">
+            <img class="item5" src="assets/pictures/Cookie.png" alt="">
+            <img class="item6" src="assets/pictures/Cookie.png" alt="">
+            <img class="item7" src="assets/pictures/Cookie.png" alt="">
+            <img class="item8" src="assets/pictures/Cookie.png" alt="">
+            <img class="item9" src="assets/pictures/Cookie.png" alt="">
+        </div>
     </div>
 </body>
 <script src="javascript/main.js"></script>
